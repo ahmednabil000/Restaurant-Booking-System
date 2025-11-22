99c8479526258866bf44869bc4b152cb4a753d95
Ahmed Nabil <ahmednabil3500@gmail.com>
2025-11-22 21:24:41 +0200

    fixing some bugs in navigation in home page

--- resturant-app/src/ui/InformationCard.jsx
+++ resturant-app/src/ui/InformationCard.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router";
+
 const InformationCard = ({ children, title, content }) => {
   return (
-    <button className="flex cursor-pointer flex-col gap-4 sm:gap-6 border-2 border-gray-400/10 p-4 sm:p-6 md:p-8 rounded-lg justify-center items-center w-full h-full min-h-48 sm:min-h-56 md:min-h-64 hover:shadow-lg transition-shadow duration-200">
+    <Link
+      to={"/reserve"}
+      className="flex cursor-pointer flex-col gap-4 sm:gap-6 border-2 border-gray-400/10 p-4 sm:p-6 md:p-8 rounded-lg justify-center items-center w-full h-full min-h-48 sm:min-h-56 md:min-h-64 hover:shadow-lg transition-shadow duration-200"
+    >
       <div className="shrink-0">{children}</div>
       <span className="text-xl sm:text-2xl md:text-3xl font-semibold text-center">
         {title}
@@ -8,7 +13,7 @@ const InformationCard = ({ children, title, content }) => {
       <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-[#565d6d] text-center leading-relaxed">
         {content}
       </p>
-    </button>
+    </Link>
   );
 };
 
