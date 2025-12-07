ca0942354d1b492131f62296e8afcc03423086ce
Ahmed Nabil <ahmednabil3500@gmail.com>
2025-12-07 08:16:14 +0200

    Keep backend only

--- resturant-app/src/ui/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Container = ({ children }) => {
-  return (
-    <div className="mx-4 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-20">{children}</div>
-  );
-};
-
-export default Container;
--- resturant-app/src/ui/Seperator.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Seperator = () => {
-  return <div className="w-full py-10"></div>;
-};
-
-export default Seperator;
