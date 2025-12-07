d6be9fd6709b4c8151c28b913e489d42e95668b2
Ahmed Nabil <80886379+ahmednabil000@users.noreply.github.com>
2025-12-07 19:02:52 +0200

    Add HashRouter to main.jsx

--- resturant-app-frontend/src/main.jsx
+++ resturant-app-frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import { RouterProvider ,HashRouter} from "react-router/dom";
 import Home from "./pages/Home.jsx";
 import MainLayout from "./ui/Layouts/MainLayout.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
