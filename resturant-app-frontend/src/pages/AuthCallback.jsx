98fabb819af57bd5c835ff87e688031226c46b1d
Ahmed Nabil <80886379+ahmednabil000@users.noreply.github.com>
2025-12-07 17:53:24 +0200

    Update AuthCallback.jsx

--- resturant-app-frontend/src/pages/AuthCallback.jsx
+++ resturant-app-frontend/src/pages/AuthCallback.jsx
@@ -43,7 +43,7 @@ const AuthCallback = () => {
                   type: "GOOGLE_AUTH_ERROR",
                   message: "خطأ في معالجة بيانات المستخدم",
                 },
-                window.location.origin
+               "https://restaurant-booking-system-3.onrender.com"
               );
               window.close();
             }
@@ -57,7 +57,7 @@ const AuthCallback = () => {
                 type: "GOOGLE_AUTH_ERROR",
                 message: errorMessage,
               },
-              window.location.origin
+                "https://restaurant-booking-system-3.onrender.com"
             );
             window.close();
           }
@@ -70,7 +70,7 @@ const AuthCallback = () => {
               type: "GOOGLE_AUTH_ERROR",
               message: "حدث خطأ أثناء معالجة المصادقة",
             },
-            window.location.origin
+             "https://restaurant-booking-system-3.onrender.com"
           );
           window.close();
         }
